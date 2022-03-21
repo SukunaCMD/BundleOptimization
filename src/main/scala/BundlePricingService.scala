029f9fe4d5d82e3ad9e79eba0d73b309eeba7f5b
SukunaCMD <81257997+SukunaCMD@users.noreply.github.com>
2022-03-21 10:24:04 -0400

    search fn updated, uses more appropiate combinator exists & contains rather than foreach.

--- src/main/scala/BundlePricingService.scala
+++ src/main/scala/BundlePricingService.scala
@@ -60,38 +60,34 @@ class BundlePricingService(catalog: Seq[CatalogItem], bundlePromotions: Seq[Bund
   def applyTPD(p: BundlePromotions.BundleTotalPriceDiscount, cart: Seq[CartItem]): Applied = {
 
     val promoItem = p.cartItems.head // only n=1 for a total price discount
+
     def loop(curCart: Seq[CartItem], i: Int): Applied = curCart.toList match {
       case Nil => Applied(cart, Price(0))
       case cartHead :: _ if cartHead.catalogItem==promoItem.catalogItem =>
         if(cartHead.quantity.value>=promoItem.quantity.value) {
           val applyTimesX = cartHead.quantity.value/promoItem.quantity.value
           val leftOver = cartHead.quantity.value%promoItem.quantity.value
-
           val totaled = applyTimesX * p.totalDiscountedPrice.value
+
           if(leftOver>=1) {
             val newCart = cart.updated(i, CartItem(cartHead.catalogItem, Quantity(leftOver)))
             Applied(newCart, Price(totaled)).add(loop(newCart, i))
-          }
-          else {
+          } else {
             Applied(cart.filter(_!=cartHead), Price(totaled)) // deleted, inefficient cause linear search
           }
-        }
-        else {
+        } else {
           Applied(curCart, cartItemPrice(cartHead))
         }
       case _ :: t => loop(t, i+1)
     }
     loop(cart, 0)
   }
+  
 
   def isPromoItem(p: BundlePromotions.BundleDiscountOnItemUnitPrice, item: CartItem): Boolean = {
-    p.cartItems.foreach{promoItem =>
-      if(item.catalogItem==promoItem.catalogItem) {
-        return true
-      }
-    }
-    false
+    p.cartItems.exists(_.catalogItem == item.catalogItem)
   }
+
 
   def applyOnPrice(p: BundlePromotions.BundleDiscountOnItemUnitPrice, cart: Seq[CartItem]): Applied = cart.toList match {
     case Nil => Applied(cart, Price(0))
@@ -108,14 +104,12 @@ class BundlePricingService(catalog: Seq[CatalogItem], bundlePromotions: Seq[Bund
       if(cartItem.catalogItem==item.catalogItem) {
         val leftOver = cartItem.quantity.value - item.quantity.value
         if(leftOver>0) {
-          foldingCart:+CartItem(item.catalogItem, Quantity(leftOver))
-        }
-        else {
+          foldingCart :+ CartItem(item.catalogItem, Quantity(leftOver))
+        } else {
           foldingCart
         }
-      }
-      else {
-        foldingCart:+cartItem
+      } else {
+        foldingCart :+ cartItem
       }
     }
 
@@ -125,9 +119,9 @@ class BundlePricingService(catalog: Seq[CatalogItem], bundlePromotions: Seq[Bund
     case Nil => Applied(cart, price)
     case h :: t => applyFoundOnUnit(t, remove(h,cart), price)
   }
-  
-  def exists(item: CartItem)= catalog.contains(item.catalogItem)
 
+  def exists(item: CartItem)= catalog.contains(item.catalogItem)
+                                                                                                                   
   def isCartValid(cart: Cart): Boolean = {
     cart.cartItems.foreach{
       a => if(!exists(a)) return false
